c1d3e7ac0ad7fd5e0452a0594d9f6bdb211a1f82
sudhabisht <sudha.bisht@webaccess.co.in>
2014-10-17 15:31:51 +0530

    Pricset Modification

--- js/quickdonate.js
+++ js/quickdonate.js
@@ -11,4 +11,65 @@
       });
     }
   ]);
+
+  quickDonation.factory('priceListService', function($q) {
+    return {
+      getpriceList:function() {
+        var deferred = $q.defer();
+        var priceList= [];
+        var contributionId;
+
+        //get contribution id
+        CRM.api3('Setting', 'get', {
+          "sequential": 1,
+          "return": "quick_donation_page"
+        }).done(function(result) {
+          contributionId = result.values[0]['quick_donation_page'];
+        });
+
+        //get priceSetId and Amount List
+        CRM.api3('PriceSet', 'get', {
+          "sequential": 1,
+        }).done(function(result) {
+          $.each(result.values, function(key, value) {
+            if (value.entity['civicrm_contribution_page'] && ($.inArray(contributionId, value.entity['civicrm_contribution_page']) >=0 )) {
+              CRM.api3('PriceField', 'get', {
+                "sequential": 1,
+                "price_set_id": value.id,
+              }).done(function(fieldVal) {
+                $.each(fieldVal.values, function(fieldKey, fieldVal){
+                  CRM.api3('PriceFieldValue', 'get', {
+                    "sequential": 1,
+                    "price_field_id": fieldVal.id,
+                  }).done(function(amtList) {
+                    $.each(amtList.values, function(amtkey, amtval) {
+                      priceList.push(amtval);
+                    });
+                    deferred.resolve(priceList);
+                  });
+                });
+              });
+            }
+          });
+        });
+        return deferred.promise;
+      }
+    };
+  });
+
+  quickDonation.controller('QuickDonationCtrl', function($scope, priceListService) {
+    priceListService.getpriceList().then(function(data){$scope.priceListInfo = data;});
+    $scope.hideValue=true;
+    $scope.amountSelected = function(price) {
+      $scope.amount = price;
+    }
+    $scope.amountActive = function(price) {
+      return $scope.amount === price;
+    }
+    $scope.amounthover = function(price) {
+      $scope.message = price;
+      $scope.hideValue = false;
+      return $scope.message;
+    }
+  });
 })(angular, CRM.$, CRM._);
