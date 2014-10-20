d8a9827d2741e2308491a6229b8bf973961b4eb4
sudhabisht <sudha.bisht@webaccess.co.in>
2014-10-20 12:50:27 +0530

    Additional changes

--- js/quickdonate.js
+++ js/quickdonate.js
@@ -15,44 +15,44 @@
   quickDonation.factory('priceListService', function($q) {
     return {
       getpriceList:function() {
-        var deferred = $q.defer();
+        var holdPriceList = $q.defer();
         var priceList= [];
         var contributionId;
 
         //get contribution id
         CRM.api3('Setting', 'get', {
-          "sequential": 1,
           "return": "quick_donation_page"
         }).done(function(result) {
-          contributionId = result.values[0]['quick_donation_page'];
+          contributionId = result.values[result.id]['quick_donation_page'];
         });
 
         //get priceSetId and Amount List
         CRM.api3('PriceSet', 'get', {
           "sequential": 1,
+          "extends":"CiviContribute",
         }).done(function(result) {
           $.each(result.values, function(key, value) {
-            if (value.entity['civicrm_contribution_page'] && ($.inArray(contributionId, value.entity['civicrm_contribution_page']) >=0 )) {
+            if ($.inArray(contributionId, value.entity['civicrm_contribution_page']) >=0 ) {
               CRM.api3('PriceField', 'get', {
                 "sequential": 1,
                 "price_set_id": value.id,
               }).done(function(fieldVal) {
-                $.each(fieldVal.values, function(fieldKey, fieldVal){
+                $.each(fieldVal.values, function(fieldKey, fieldVal) {
                   CRM.api3('PriceFieldValue', 'get', {
                     "sequential": 1,
                     "price_field_id": fieldVal.id,
                   }).done(function(amtList) {
-                    $.each(amtList.values, function(amtkey, amtval) {
-                      priceList.push(amtval);
+                    $.each(amtList.values, function(amtKey, amtVal) {
+                      priceList.push(amtVal);
                     });
-                    deferred.resolve(priceList);
+                    holdPriceList.resolve(priceList);
                   });
                 });
               });
             }
           });
         });
-        return deferred.promise;
+        return holdPriceList.promise;
       }
     };
   });
@@ -60,22 +60,23 @@
 
 
   quickDonation.controller('QuickDonationCtrl', function($scope, priceListService) {
-    priceListService.getpriceList().then(function(data){$scope.priceListInfo = data;});
+    priceListService.getpriceList().then(function(data) {
+      $scope.priceListInfo = data;
+    });
+
     $scope.hidePriceVal=true;
     $scope.amountSelected = function(price) {
-    $scope.amount = price;
+      $scope.amount = price;
     }
-    $scope.amountActive = function(price) {
-      return $scope.amount === price;
-    }
+
     $scope.amounthover = function(price) {
     $scope.message = price;
     $scope.hidePriceVal = false;
       return $scope.message;
     }
 
-      //manually binds Parsley--Validation Library to this form.
-      $('#quickDonationForm').parsley();
-      $scope.formInfo = {}; //property is set to bind input value
-    });
+    //manually binds Parsley--Validation Library to this form.
+    $('#quickDonationForm').parsley();
+    $scope.formInfo = {}; //property is set to bind input value
+  });
 })(angular, CRM.$, CRM._);
